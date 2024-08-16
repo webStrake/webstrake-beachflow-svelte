dccb1c2e7258e8d9fcaaf9a8e01bd37f1bf8ca08
Harsh Kothari <techsmash88@gmail.com>
2024-08-16 18:31:23 +0530

    new build

--- dist/Input.svelte
+++ dist/Input.svelte
@@ -8,12 +8,15 @@ export let required = false;
 if (id === "") {
   id = Math.random().toString(36).substring(7);
 }
+function typeAction(node) {
+  node.type = type;
+}
 </script>
 
 <div class="input-container">
   {#if icon}
     <span class="input-icon">{icon}</span>
   {/if}
-  <input {id} {type} class="input-field" {placeholder} {value} {required} />
+  <input use:typeAction {id} class="input-field" {placeholder} bind:value {required} />
   <label for="{id}" class="input-label">{label}</label>
 </div>
