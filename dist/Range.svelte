30fa3f80ba2ad97883ad4e2c15947e7125b01527
Harsh Kothari <techsmash88@gmail.com>
2024-08-20 23:39:25 +0530

    new build

--- dist/Range.svelte
+++ dist/Range.svelte
@@ -6,21 +6,24 @@ export let color = "primary";
 export let size = "md";
 export let showValue = false;
 $: percentage = (value - min) / (max - min) * 100;
+$: thumbPosition = `calc(${percentage}% + (${8 - percentage * 0.16}px))`;
 </script>
-  
-  <div class="range-container {size}">
-    <input
+
+<div class="range-container {size}">
+  <input
       type="range"
       bind:value
       {min}
       {max}
       {step}
       class="range {color}"
-    />
-    <div class="range-track">
+  />
+  <div class="range-track">
       <div class="range-fill {color}" style="width: {percentage}%"></div>
-    </div>
-    {#if showValue}
-      <div class="range-value">{value}</div>
-    {/if}
   </div>
+  <div class="range-thumb {color}" style="left: {thumbPosition}">
+      {#if showValue}
+          <div class="range-value">{value}</div>
+      {/if}
+  </div>
+</div>
